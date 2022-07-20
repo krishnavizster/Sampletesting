925324f55a33e1b05ec7c863f7d6ab50cede0f07
unknown <krishnavizster@gmail.com>
2022-07-20 07:10:38 -0700

    datainjectioncodeupdated

---
+++ storessales/component/data_injection.py
@@ -0,0 +1,38 @@
+from storessales.entity.config_entity import DataIngestionConfig
+import sys,os
+from storessales.exception import StoressalesException
+from storessales import logger
+from storessales.logger import logging
+
+class DataIngestion:
+
+    def __init__(self,data_ingestion_config:DataIngestionConfig ):
+        try:
+            logging.info(f"{'>>'*20}Data Ingestion log started.{'<<'*20} ")
+            self.data_ingestion_config = data_ingestion_config
+
+        except Exception as e:
+            raise StoressalesException(e,sys)
+
+    
+    def download_housing_data(self,) -> str:
+        try:
+            #extraction remote url to download dataset
+            download_url = self.data_ingestion_config.dataset_download_url
+
+            #folder location to download file
+            tgz_download_dir = self.data_ingestion_config.tgz_download_dir
+            
+            os.makedirs(tgz_download_dir,exist_ok=True)
+
+            housing_file_name = os.path.basename(download_url)
+
+            tgz_file_path = os.path.join(tgz_download_dir, housing_file_name)
+
+            logging.info(f"Downloading file from :[{download_url}] into :[{tgz_file_path}]")
+            urllib.request.urlretrieve(download_url, tgz_file_path)
+            logging.info(f"File :[{tgz_file_path}] has been downloaded successfully.")
+            return tgz_file_path
+
+        except Exception as e:
+            raise StoressalesException(e,sys) from e
---
+++ storessales/util/util.py
@@ -0,0 +1,19 @@
+import yaml
+from storessales.exception import StoressalesException
+import os,sys
+
+
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file 
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise StoressalesException(e,sys) from e
+
