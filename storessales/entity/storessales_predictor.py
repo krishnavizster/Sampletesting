67dec514f967546df28d00421e6a90edef90b6dc
unknown <krishnavizster@gmail.com>
2022-07-31 06:20:58 -0700

    predictors

--- storessales/entity/storessales_predictor.py
+++ storessales/entity/storessales_predictor.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from numpy import int64
+
 from storessales.exeception import StoressalesExeception
 from storessales.util.util import load_object
 
@@ -10,51 +12,108 @@ import pandas as pd
 class StoressalesData:
 
     def __init__(self,
-                 longitude: float,
-                 latitude: float,
-                 housing_median_age: float,
-                 total_rooms: float,
-                 total_bedrooms: float,
-                 population: float,
-                 households: float,
-                 median_income: float,
-                 ocean_proximity: str,
-                 median_house_value: float = None
+                 Item_Identifier : str,
+                 Item_Weight : float,
+                 Item_Fat_Content  : str,
+                 Item_Visibility: float,
+                 Item_Type : str,
+                 Item_MRP: float,
+                 Outlet_Identifier: str,
+                 Outlet_Establishment_Year : int64,
+                 Outlet_Size : str,
+                 Outlet_Location_Type: str = None
+                 Outlet_Type : str,
+                 Item_Outlet_Sales :float,
+
+
                  ):
         try:
-            self.longitude = longitude
-            self.latitude = latitude
-            self.housing_median_age = housing_median_age
-            self.total_rooms = total_rooms
-            self.total_bedrooms = total_bedrooms
-            self.population = population
-            self.households = households
-            self.median_income = median_income
-            self.ocean_proximity = ocean_proximity
-            self.median_house_value = median_house_value
+            self.Item_Identifier = Item_Identifier
+            self.Item_Weight  = Item_Weight 
+            self.Item_Fat_Content   = Item_Fat_Content  
+            self.Item_Visibility = Item_Visibility
+            self.Item_Type  = Item_Type 
+            self.Item_MRP = Item_MRP
+            self.Outlet_Identifier = Outlet_Identifier
+            self.Outlet_Establishment_Year  = Outlet_Establishment_Year 
+            self.Outlet_Size  = Outlet_Size 
+            self.Outlet_Location_Type = Outlet_Location_Type
+            self.Outlet_Type = OuOutlet_Type
+            self.Item_Outlet_Sales = Item_Outlet_Sales
+
+
         except Exception as e:
             raise StoressalesExeception(e, sys) from e
 
-    def get_housing_input_data_frame(self):
+#.......
+#Item_Identifier               object
+#Item_Weight                  float64
+#Item_Fat_Content              object
+#Item_Visibility              float64
+#Item_Type                     object
+#Item_MRP                     float64
+#Outlet_Identifier             object
+#Outlet_Establishment_Year      int64
+#Outlet_Size                   object
+#Outlet_Location_Type          object
+#Outlet_Type                   object
+#Item_Outlet_Sales            float64
+#......
+
+
+#..........
+#columns:
+
+  #Item_Weight:float64
+  #Item_Visibility:float64
+  #Item_MRP:float64
+  
+#numerical_columns:
+
+#Outlet_Establishment_Year:int64 
+
+#categorical_columns:
+
+#Item_Identifier:object 
+##Item_Fat_Content:object
+#Item_Type:object
+#Outlet_Identifier:object
+#Outlet_Size:object 
+#Outlet_Location_Type:object 
+#Outlet_Type:object 
+#stores:Category 
+
+#target_column:   Item_Outlet_Sales:float64
+#..........
+
+    def get_storessales_input_data_frame(self):
 
         try:
-            storessales_input_dict = self.get_housing_data_as_dict()
+            storessales_input_dict = self.get_storessales_data_as_dict()
             return pd.DataFrame(storessales_input_dict)
         except Exception as e:
             raise StoressalesExeception(e, sys) from e
 
-    def get_housing_data_as_dict(self):
+    def get_storessales_data_as_dict(self):
         try:
             input_data = {
-                "longitude": [self.longitude],
-                "latitude": [self.latitude],
-                "housing_median_age": [self.housing_median_age],
-                "total_rooms": [self.total_rooms],
-                "total_bedrooms": [self.total_bedrooms],
-                "population": [self.population],
-                "households": [self.households],
-                "median_income": [self.median_income],
-                "ocean_proximity": [self.ocean_proximity]}
+                "Item_Identifier": [self.Item_Identifier],
+                "Item_Weight": [self.Item_Weight],
+                "Item_Fat_Content": [self.Item_Fat_Content],
+                "Item_Visibility": [self.Item_Visibility],
+                "Item_Type": [self.Item_Type],
+                "Item_MRP": [self.Item_MRP],
+                "Outlet_Identifier": [self.Outlet_Identifier],
+                "Outlet_Establishment_Year ": [self.Outlet_Establishment_Year ],
+                "Outlet_Size": [self.Outlet_Size]
+                "Outlet_Location_Type": [self.Outlet_Location_Type],
+                "Outlet_Type": [self.Outlet_Type],
+                "Item_Outlet_Sales": [self.Item_Outlet_Sales],
+
+
+
+                
+                }
             return input_data
         except Exception as e:
             raise StoressalesExeception(e, sys)
